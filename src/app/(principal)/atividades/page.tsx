48f5c1a80b57a20214027480bef9580c2d95023d
Miguel <75583778+Miguel03706@users.noreply.github.com>
2024-09-04 10:29:03 -0300

    arrumando menu principal

--- src/app/(principal)/atividades/page.tsx
+++ src/app/(principal)/atividades/page.tsx
@@ -1,31 +1,32 @@
 import React from 'react'
-import MenuLateral from '@/components/Menu-lateral/Menu'
 import SectionActivities from '@/components/SectionActivities/SectionActivities'
-import SectionInfo from '@/components/SectionInfo/SectionInfo'
-import StackItens from '@/components/Menu-lateral/StackItens/StackItens'
 import StackItem from '@/components/Menu-lateral/StackItens/StackItem/StackItem'
-import InicioIcon from 'public/icons/inicio.webp'
-import MissionIcon from 'public/icons/missoes.webp'
-import StoreIcon from 'public/icons/loja.webp'
-import ConfigIcon from 'public/icons/config.webp'
+import StackItens from '@/components/Menu-lateral/StackItens/StackItens'
+import SectionInfo from '@/components/SectionInfo/SectionInfo'
+import MenuLateral from '@/components/Menu-lateral/Menu'
 import PerfilIcon from 'public/icons/logo_urso.webp'
-
+import MissionIcon from 'public/icons/missoes.webp'
+import ConfigIcon from 'public/icons/config.webp'
+import InicioIcon from 'public/icons/inicio.webp'
+import StoreIcon from 'public/icons/loja.webp'
 
 export default function Atividades() {
   return (
     <MenuLateral>
       <StackItens>
-        <StackItem icon={InicioIcon}>Inicio</StackItem>
-        <StackItem icon={MissionIcon}>Missões</StackItem>
-        <StackItem icon={StoreIcon}>Loja</StackItem>
-        <StackItem icon={ConfigIcon}>Configurações</StackItem>
-        <StackItem icon={PerfilIcon}>Perfil</StackItem>
-
+        <StackItem icon={InicioIcon} redirect='/atividades'>Inicio</StackItem>
+        <StackItem icon={MissionIcon} redirect='/missoes'>Missões</StackItem>
+        <StackItem icon={StoreIcon} redirect='/loja'>Loja</StackItem>
+        <StackItem icon={ConfigIcon} redirect='/configuracoes'>Configurações</StackItem>
+        <StackItem icon={PerfilIcon} redirect='/perfil'>Perfil</StackItem>
       </StackItens>
-      <SectionActivities>
-        <div>Atividades</div>
-      </SectionActivities>
-      <SectionInfo />
+      <div>
+        <>teste</>
+        <SectionActivities>
+          <div className='h-screen'>Atividades</div>
+        </SectionActivities>
+        <SectionInfo />
+      </div>
     </MenuLateral>
   )
 }
--- src/components/Menu-lateral/StackItens/StackItem/StackItem.tsx
+++ src/components/Menu-lateral/StackItens/StackItem/StackItem.tsx
@@ -5,12 +5,13 @@ import React from 'react'
 interface MenuItemProps {
     children: React.ReactNode
     icon: StaticImageData
+    redirect?: string
 }
 
 export default function StackItem(props: MenuItemProps) {
     return (
         <div className='stack-item'>
-            <Link href='/painel/cadastro'>
+            <Link href={`${props.redirect}`}>
                 <Image src={props.icon} alt='' className='icon' />
                 <span className='title'>{props.children}</span>
             </Link>
