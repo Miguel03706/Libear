4704bcc42eb2a123c4ecb93e0ab98870f4c9f850
Miguel <75583778+Miguel03706@users.noreply.github.com>
2024-09-05 00:41:48 -0300

    teste

---
+++ src/@types/activities.ts
@@ -0,0 +1,8 @@
+export type Activity = {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+    link: string
+    progress: number;
+}
--- src/components/SectionActivities/ActivitiesRoad/ActivitiesRoad.tsx
+++ src/components/SectionActivities/ActivitiesRoad/ActivitiesRoad.tsx
@@ -2,9 +2,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Activities from './activities.json'
 import "./styles.scss"
+import { Image } from '@chakra-ui/react';
 
 export default function ActivitiesRoad() {
-    const [color, setColor] = useState('');
+    const [color, setColor] = useState('#E5DE2F');
+
+    useEffect(() => {
+    }, [])
 
     useEffect(() => {
         if (localStorage.getItem('chakra-ui-color-mode') == "dark") {
@@ -21,7 +25,13 @@ export default function ActivitiesRoad() {
                 Activities.map((activity, index) => {
                     return (
                         <div key={activity.id} className="list-activities">
-                            
+                            <div className='lesson'>
+                                <div className="progress-bar" style={{
+                                    "--progress": `${activity.progress}`
+                                } as React.CSSProperties}>
+                                    <Image src={`/icons/activitys/${activity.image}.webp`} alt={activity.title} w={84} h={84} />
+                                </div>
+                            </div>
                         </div>
                     )
                 })
