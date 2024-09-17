b823efdbb69040774ff665cab1b2c38dffc8c8cb
Miguel <75583778+Miguel03706@users.noreply.github.com>
2024-09-17 14:11:46 -0300

    activities road feito

--- src/components/SectionActivities/ActivitiesRoad/ActivitiesRoad.tsx
+++ src/components/SectionActivities/ActivitiesRoad/ActivitiesRoad.tsx
@@ -1,19 +1,42 @@
 'use client'
-import React, { useEffect, useRef, useState } from 'react'
-import Activities from './activities.json'
-import "./styles.scss"
+import React, { useState, useEffect } from 'react';
+import "./styles.scss";
 import { Image } from '@chakra-ui/react';
+import Activities from './activities.json'
 
 export default function ActivitiesRoad() {
     const [color, setColor] = useState('#E5DE2F');
 
-    useEffect(() => {
-    }, [])
+    const contentStyle = (index: number) => {
+        const positionInGroup = index % 4;
+        const baseMargin = 2; // Base margin increment
+        let marginLeft = '0rem';
+        let marginRight = '0rem';
+
+        if (positionInGroup === 0) {
+            // Primeiro item centralizado
+            marginLeft = 'auto';
+            marginRight = 'auto';
+        } else if (positionInGroup === 1 || positionInGroup === 2) {
+            // Segundo e terceiro itens vão para a direita
+            marginLeft = `${positionInGroup * baseMargin}rem`;
+        } else if (positionInGroup === 3) {
+            // Quarto item vai um pouco mais para a esquerda
+            marginLeft = `${(positionInGroup - 1) * baseMargin - 1}rem`;
+        }
+
+        return {
+            marginLeft,
+            marginRight,
+        };
+    };
 
     useEffect(() => {
-        if (localStorage.getItem('chakra-ui-color-mode') == "dark") {
+    }, []);
+
+    useEffect(() => {
+        if (localStorage.getItem('chakra-ui-color-mode') === "dark") {
             setColor('#49CFE1');
-
         } else {
             setColor('#E5DE2F');
         }
@@ -24,18 +47,16 @@ export default function ActivitiesRoad() {
             {
                 Activities.map((activity, index) => {
                     return (
-                        <div key={activity.id} className="list-activities">
-                            <div className='lesson'>
-                                <div className="progress-bar" style={{
-                                    "--progress": `${activity.progress}`
-                                } as React.CSSProperties}>
+                        <div className="roadmap" key={activity.id}>
+                            <div className={`roadmap-item ${index % 2 ? 'left' : 'right'}`} style={contentStyle(index)}>
+                                <div className="content">
                                     <Image src={`/icons/activitys/${activity.image}.webp`} alt={activity.title} w={84} h={84} />
                                 </div>
                             </div>
                         </div>
-                    )
+                    );
                 })
             }
-        </div >
-    )
+        </div>
+    );
 }
